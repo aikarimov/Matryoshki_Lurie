8a9c5913285265251704d410b2077611392087ea
Artur Karimov <64038829+aikarimov@users.noreply.github.com>
2025-09-08 17:06:16 +0300

    Update Test_initial.m

--- Test_initial.m
+++ Test_initial.m
@@ -3,8 +3,6 @@ close all
 x0 = [0.3483; 0.3992; -0.6120];
 y0 = -x0;
 
-%fzero(@phi_0,1)
-
 h =  0.01;
 T = 700;
 tspan = 0:h:T;
@@ -13,8 +11,6 @@ alph = 0.2;
 ctr = 1;
 [~,y1] = ode89(@finitial,tspan,x0);
 [~,y2] = ode89(@finitial,tspan,y0);
- % y1 = myRK4(@finitial,tspan,x0);
- % y2 = myRK4(@finitial,tspan,y0);
 
 figure(1);
 subplot(1,2,1);
@@ -22,7 +18,6 @@ col = [cmap(ctr,:),alph];
 plot(y1(:,1),y1(:,2),'Color',col); hold on
 col = [cmap(ctr+1,:),alph];
 plot(y2(:,1),y2(:,2),'Color',col);
-%axis equal
 
 xlabel('$x$','interpreter','latex');
 ylabel('$y$','interpreter','latex');
@@ -51,20 +46,3 @@ plot3(y1(:,1),y1(:,2),y1(:,3),'Color',col); hold on
 col = [cmap(ctr+1,:),alph];
 plot3(y2(:,1),y2(:,2),y2(:,3),'Color',col);
 
-% figure(3);
-% c = [-19; -3.51;  -3.2]; 
-% sig1 = c'*y1';
-% sig2 = c'*y2';
-% 
-% p1 = phi_0(sig1);
-% p2 = phi_0(sig2);
-% 
-% col = [cmap(ctr,:)];
-% plot(sig1,p1,'Color',col); hold on
-% col = [cmap(ctr+1,:)];
-% plot(sig2,p2,'Color',col); hold on
-% xlabel('$\sigma$','interpreter','latex');
-% ylabel('$\varphi_0(\sigma)$','interpreter','latex');
-% set(gca,'TickLabelInterpreter','latex');
-% xtickformat('$%g$'); 
-% ytickformat('$%g$');
